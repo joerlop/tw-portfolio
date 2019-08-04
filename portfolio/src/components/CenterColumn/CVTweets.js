095931268cbd927362a1fd551405033b9b344f55
joerlop <jj.erlich155@gmail.com>
2019-08-04 15:48:56 -0500

    Social media icon like developed and working

--- portfolio/src/components/CenterColumn/CVTweets.js
+++ portfolio/src/components/CenterColumn/CVTweets.js
@@ -3,24 +3,26 @@ import "./CenterColumn.scss";
 
 import TweetProfilePic from "./TweetProfilePic";
 import TweetHeader from "./TweetHeader";
-import TweetSocial from "./TweetSocial"
+import TweetSocial from "./TweetSocial";
 
 class CVTweets extends React.Component {
   render() {
     return (
       <>
         <div className="tweet">
-          <TweetProfilePic />
-          <div className="tweet-content">
-            <TweetHeader date="Oct 31, 2008" />
-            <div className="tweet-message">
-              <p>
-                Unpopular? Opinion: #Bitcoin's biggest winners are going to be
-                the longest hodlers, not necessarily the early adopters...
-              </p>
+          <div className="tweet-upper">
+            <TweetProfilePic />
+            <div className="tweet-content">
+              <TweetHeader date="Oct 31, 2008" />
+              <div className="tweet-message">
+                <p>
+                  Unpopular? Opinion: #Bitcoin's biggest winners are going to be
+                  the longest hodlers, not necessarily the early adopters...
+                </p>
+              </div>
             </div>
-            <TweetSocial />
           </div>
+          <TweetSocial />
         </div>
       </>
     );
--- portfolio/src/components/CenterColumn/TweetSocial.js
+++ portfolio/src/components/CenterColumn/TweetSocial.js
@@ -1,21 +1,45 @@
 import React from "react";
 import "./CenterColumn.scss";
 
-import { ReactComponent as HeartIcon } from "../../images/like.svg";
+import { ReactComponent as HeartIcon } from "../../images/heart.svg";
+import { ReactComponent as HeartIconFilled } from "../../images/heart-filled.svg";
 
 class TweetSocial extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      likes: 100
-    }
+      likes: 100,
+      liked: false
+    };
   }
+
+  like = () => {
+    if (this.state.liked) {
+      this.setState({
+        likes: this.state.likes - 1,
+        liked: !this.state.liked
+      });
+    } else {
+      this.setState({
+        likes: this.state.likes + 1,
+        liked: !this.state.liked
+      });
+    }
+  };
 
   render() {
     return (
-      <div className="tweet-social">
-        <HeartIcon className="heart-icon" />
-        <p>{this.state.likes}</p>
+      <div onClick={this.like} className="tweet-social">
+        <div className="like">
+          <div className="icon-container">
+            {this.state.liked ? (
+              <HeartIconFilled className="heart-icon-filled" />
+            ) : (
+              <HeartIcon className="heart-icon" />
+            )}
+          </div>
+          <p className={`liked-${this.state.liked}`}>{this.state.likes}</p>
+        </div>
       </div>
     );
   }
