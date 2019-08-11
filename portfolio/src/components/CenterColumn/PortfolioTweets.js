d43a19b608067e64076734d00a9e7416f69974b1
joerlop <jj.erlich155@gmail.com>
2019-08-11 14:35:06 -0500

    Added first version of portfolio

--- portfolio/src/components/CenterColumn/PortfolioTweets.js
+++ portfolio/src/components/CenterColumn/PortfolioTweets.js
@@ -1,13 +1,57 @@
 import React from "react";
 import "./CenterColumn.scss";
-
 import profileImage from "../../images/jonathan.jpg";
+import diarioLogo from "../../images/diario3.jpg"
+
+import TweetProfilePic from "./TweetProfilePic";
+import TweetHeader from "./TweetHeader";
+import TweetSocial from "./TweetSocial";
 
 class PortfolioTweets extends React.Component {
+  convertToLink = (href, message) => {
+    return (
+      <a target="_blank" href={href}>
+        {message}
+      </a>
+    );
+  };
+
   render() {
     return (
       <>
-        <p>Portfolio</p>
+        <div className="tweet">
+          <div className="tweet-upper">
+            <TweetProfilePic />
+            <div className="tweet-content">
+              <TweetHeader date="June 2 2019 - July 6, 2019" />
+              <div className="tweet-message">
+                <p>
+                  Built my first app (solo) since joining Lambda School in
+                  March.
+                  <br />
+                  <br />
+                  {this.convertToLink("https://wearediario.com/", "Diario")} is
+                  a simple, fun and private diary built on{" "}
+                  {this.convertToLink("https://blockstack.org/", "#Blockstack")}{" "}
+                  using web 3.0 principles - we will never have access to what
+                  you write.
+                  <br />
+                  <br />
+                  Built with: React, Redux, GSAP and Blockstack.
+                  <br />
+                  <br />
+                  More project info. (including inspiration){" "}
+                  {this.convertToLink(
+                    "https://devpost.com/software/diario",
+                    "here"
+                  )}.
+                </p>
+                <img src={diarioLogo} />
+              </div>
+            </div>
+          </div>
+          <TweetSocial likes={100} />
+        </div>
       </>
     );
   }
